ab353572e03533625b8e7cf69faa77c813bc4e25
jet1202 <jet86488@gmail.com>
2023-04-14 17:01:02 +0900

    SlideMaintain 1

--- Assets/Scripts/SlideMaintainData.cs
+++ Assets/Scripts/SlideMaintainData.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class SlideMaintainData : MonoBehaviour
 {
+    [SerializeField] private GameEvent gameEvent;
+    [SerializeField] private CenterDirector centerDirector;
+    
     void Start()
     {
         
